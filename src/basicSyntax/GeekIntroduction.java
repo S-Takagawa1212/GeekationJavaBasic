cd8761ffaed797ba726754a5b634c5624d6b3c16
S-Takagawa <s.takagawa.cingroup2012@gmail.com>
2025-04-03 23:08:36 +0900

    feat(GeekIntroduction): 数値のバリデーションルールを実装
    コミットの粒度が少し大きいので今後は小さくする。

---
+++ src/basicSyntax/GeekIntroduction.java
@@ -0,0 +1,52 @@
+package basicSyntax;
+
+public class GeekIntroduction {
+	
+	public static void main(String[] args) {
+		specialSkill(5,15);
+		System.out.println("pass");
+	}
+
+	protected void greeting(int age, double height) {
+		System.out.println("こんにちは! 私はGeek君でごわす。");
+		System.out.println("年は" + age + "でごわす。");
+		System.out.println("身長は" + height + "でごわす。");
+	}
+	
+	protected static void specialSkill(int num1, int num2) {
+	    judgeNumericRules(num1, num2, 300);
+		for (; num1 < num2; num1++) {
+			
+			
+		}	
+	}
+	
+	private static boolean isGreaterThanZero(int a,int b) {
+		return ((a>0) && (b>0));
+	}
+	private static boolean isGreaterThanB(int a, int b) {
+		return (a < b);
+	}
+	// 第１、２引数が第３引数以下であればFalseを返す。
+	private static boolean isEqualOrGreaterThanX(int a, int b ,int X) {
+		return (a <= X && b <= X);
+		
+	}
+	// 第１、２引数が第３引数の倍数あればTrueを返す。
+	private static boolean isMultipleOfX(int num, int X) {
+		return (num % X == 0);
+	}
+	
+	private static void judgeNumericRules(int a, int b, int X) {
+		if (!isGreaterThanZero(a, b)) {
+			throw new IllegalArgumentException("num1とnum2は0より大きい値にしてください。");
+		} else if (!isGreaterThanB(a, b)) {
+			throw new IllegalArgumentException("num2はnum1より大きい値にしてください。");
+		} else if (!isEqualOrGreaterThanX(a, b, X)) {
+			throw new IllegalArgumentException("num1とnum2は" + X + "以下にしてください。");
+		}
+	}
+	
+	
+
+}
