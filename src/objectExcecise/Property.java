93e637ad918ac7a6aba565bae9e77ae125ff2fa4
S-Takagawa <s.takagawa.cingroup2012@gmail.com>
2025-04-05 14:40:05 +0900

    feat(property): printInfo()を実装

--- src/objectExcecise/Property.java
+++ src/objectExcecise/Property.java
@@ -1,10 +1,10 @@
 package objectExcecise;
 
 public class Property {
-	protected String name;
-	protected String ownerName;
-	protected String type;
-	protected Integer price;
+	private String name;
+	private String ownerName;
+	private String type;
+	private Integer price;
 	
 	public Property(String name, String ownerName, String type, Integer price) {
 		super();
@@ -14,35 +14,45 @@ public class Property {
 		this.price = price;
 	}
 
-	public String getName() {
+	protected String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
+	protected void setName(String name) {
 		this.name = name;
 	}
 
-	public String getOwnerName() {
+	protected String getOwnerName() {
 		return ownerName;
 	}
 
-	public void setOwnerName(String ownerName) {
+	protected void setOwnerName(String ownerName) {
 		this.ownerName = ownerName;
 	}
 
-	public String getType() {
+	protected String getType() {
 		return type;
 	}
 
-	public void setType(String type) {
+	protected void setType(String type) {
 		this.type = type;
 	}
 
-	public Integer getPrice() {
+	protected Integer getPrice() {
 		return price;
 	}
 
-	public void setPrice(Integer price) {
+	protected void setPrice(Integer price) {
 		this.price = price;
 	}
+	
+	protected void printInfo() {
+		System.out.println("==============================");
+		System.out.println("物件名：" + getName());
+		System.out.println("物件所有者名：" + getOwnerName());
+		System.out.println("物件種別" + getType());
+		System.out.println("物件価格" + getPrice() + "円");
+		System.out.println("==============================");
+	}
+	
 }
