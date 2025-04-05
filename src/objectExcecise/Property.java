209cd673a844c652b9d503bba991c9721befce3d
S-Takagawa <s.takagawa.cingroup2012@gmail.com>
2025-04-05 14:13:28 +0900

    feat(property): 親クラスを作成

--- src/objectExcecise/Property.java
+++ src/objectExcecise/Property.java
@@ -1,10 +1,48 @@
 package objectExcecise;
 
 public class Property {
-
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	protected String name;
+	protected String ownerName;
+	protected String type;
+	protected Integer price;
+	
+	public Property(String name, String ownerName, String type, Integer price) {
+		super();
+		this.name = name;
+		this.ownerName = ownerName;
+		this.type = type;
+		this.price = price;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
 }
